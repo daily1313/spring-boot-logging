3d3a1b980b31efddc196bafad6e9c2b3ba7662cf
김승범 <kimsb7218@naver.com>
2024-01-17 23:57:50 +0900

    Initial commit

--- src/main/java/com/example/springbootlogging/SpringBootLoggingApplication.java
+++ src/main/java/com/example/springbootlogging/SpringBootLoggingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLoggingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLoggingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingApplication.class, args);
+    }
 
 }
